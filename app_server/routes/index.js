57b4bda84ef7b050509043fb0639744c721da5b7
@IdSundev <chefdeveloper29@gmail.com>
2020-07-08 09:20:30 +0700

    Requiring the controller files in app_server/routes/index.js

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const ctrlMain = require('../controllers/main');
+
+const ctrlLocations = require('../controllers/locations');
+const ctrlOthers = require('../controllers/others');
 
 /* GET home page. */
 router.get('/', ctrlMain.index);
