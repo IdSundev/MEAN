375563a64e03d21af8921e8b5ca7589052db3e04
@IdSundev <chefdeveloper29@gmail.com>
2020-07-09 09:11:39 +0700

    Final Schema

--- app_server/models/locations.js
+++ app_server/models/locations.js
@@ -1,5 +1,33 @@
 const mongoose = require('mongoose');
 
+const reviewSchema = new mongoose.Schema({
+  author: String,
+  rating: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 5,
+  },
+  reviewText: String,
+  createdon: {
+    type: Date,
+    'default': Date.now
+  }
+});
+
+const openingTimeSchema = new mongoose.Schema({
+  days: {
+    type: String,
+    required: true,
+  },
+  opening: String,
+  closing: String,
+  closed: {
+    type: Boolean,
+    required: true,
+  }
+});
+
 const locationSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -16,6 +44,8 @@ const locationSchema = new mongoose.Schema({
   coords: {
     type: { type: String },
     coordinates: [Number]
-  }
+  },
+  openingTimes: [openingTimeSchema],
+  reviews: [reviewSchema],
 });
 locationSchema.index({coords: '2dsphere'});
