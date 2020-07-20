0465de0e974b30937df2e845a33df5ae91f2e826
@IdSundev <chefdeveloper29@gmail.com>
2020-07-20 07:56:05 +0700

    Using class member data in the HTML template

--- app_public/src/app/home-list/home-list.component.ts
+++ app_public/src/app/home-list/home-list.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Location{
+  _id: string;
+  name: string;
+  distance: number;
+  address: string;
+  rating: number;
+  facilities: string[];
+}
+
 @Component({
   selector: 'app-home-list',
   templateUrl: './home-list.component.html',
@@ -8,6 +17,15 @@ import { Component, OnInit } from '@angular/core';
 export class HomeListComponent implements OnInit {
 
   constructor() { }
+
+  location: Location = {
+    _id: '5f10f74a0bc9ff1f95a55654',
+    name: 'Costy',
+    distance: 14.0,
+    address: 'High Street, Reading',
+    rating: 3,
+    facilities: ['hot drinks','food','power']
+  };
 
   ngOnInit(): void {
   }
